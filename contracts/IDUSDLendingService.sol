80d63aa43c82ced009525de3315c5411b728f3e8
Njoku Emmanuel <kalunjoku123@gmail.com>
2021-02-24 08:46:33 +0100

    lending service interface

---
+++ contracts/IDUSDLendingService.sol
@@ -0,0 +1,21 @@
+pragma solidity >=0.6.6;
+
+
+interface IDUSDLendingService {
+
+    function Save(uint256 amount) external;
+    
+    function Withdraw(uint256 amount) external;
+
+    function WithdrawByShares(uint256 amount, uint256 sharesAmount) external;
+
+    function WithdrawBySharesOnly(uint256 sharesAmount) external;
+
+    function GetDUSDLendingAdapterAddress() external view returns (address);
+
+    function UserShares(address user) external view returns (uint256);
+
+    function UserDUSDBalance(address user) external view returns (uint256);
+
+
+}
