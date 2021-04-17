bffee1ce4e9b7863fb8c1e092a4667802c334470
chukwuemekanweke <emekanweke604@gmail.com>
2021-04-17 20:10:54 +0100

    finished testing individual savings

--- test/XendFinanceIndividual_Yearn_V1.js
+++ test/XendFinanceIndividual_Yearn_V1.js
@@ -43,7 +43,7 @@ const daiContract = new web3.eth.Contract(DaiContractABI,DaiContractAddress);
     
 const yDaiContract = new web3.eth.Contract(YDaiContractABI,yDaiContractAddress);
 
-const unlockedAddress = "0x511ed30e9404cbec4bb06280395b74da5f876d47";
+const unlockedAddress = "0x3B15CEc2d922Ab0Ef74688bcC1056461049f89CB";
 
 
 //  Approve a smart contract address or normal address to spend on behalf of the owner
